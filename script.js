37c63bd261e1746f999c1b2142e53af1e8a68e3b
Azizat <Lazarusebi111@gmail.com>
2025-02-24 03:06:18 +0100

    'I adjusted the button timer'

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ form.addEventListener("submit", e => {
             msg.innerHTML = "Message Sent Successfully!";
             setTimeout(function(){
             msg.innerHTML = ""
-            }, 5000);
+            }, 3000);
             form.reset();
         })
         .catch(error => console.error('Error!', error.message))
